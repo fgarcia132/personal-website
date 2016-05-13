431556efdba54150860c000f5417c9580e3b1020
Francisco Garcia <ciscog11@msn.com>
2016-05-13 09:26:24 -0600

    added to interaction flow

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,11 +11,11 @@
 	</p>
 	<h2>Content Strategy</h2>
 	<P>
-		The header will have my name as well as icon links to Github, Twitter and LinkedIn. Description of what I can provide in order to sell potential clients. Contact information for any questions or inquiries. Context of who I am and why I would like to help them with their own site. The contact form will be at the bottom of the page where clients can find my email. Additionally links such as to my Github, Twitter and Linkedin will be in the footer section.
+		The header will have my name as well as icon links to Github, Twitter and LinkedIn. Description of what I can provide in order to sell potential clients. Contact information for any questions or inquiries. Context of who I am and why I would like to help them with their own site. The contact form will be at the bottom of the page where clients can find my email. Additionally links to my Github, Twitter and Linkedin will be in the footer section.
 	</P>
 	<h2>Interaction Flow</h2>
 	<p>
-		Potential client navigates to the site in order to gain more knowledge about whom they might be hiring. They read the description of services. Then flow to the next text field describing why I would like to help them with their site. Ultimately finding my contact information and professional links at the bottom of the page, and leaving an email accordingly by using the contact form.
+		Potential client navigates to the site in order to gain more knowledge about whom they might be hiring. They read the description of services. Then flow to the next text field describing why I would like to help them with their site. They click the icon links to see my professional Twitter, GitHub and LinkedIn. Ultimately using the contact for to leave an email to set up a meeting about hiring me.
 	</p>
 	<h2>Wireframes</h2>
 	<img src="images/wireframe4t.svg">
